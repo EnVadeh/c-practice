cd5cc6c4375efb4d062d2b30280b4adc430f6740
EnVadeh <87571040+EnVadeh@users.noreply.github.com>
2025-01-31 22:47:30 +0545

    linked list basic, need to add deleteNode feature too

--- algo.c
+++ algo.c
@@ -4,9 +4,8 @@
 #include <string.h>
 
 typedef struct{
-  uint64_t seed;
-  uint64_t hashValue;
-} hashTable;
+  void* mPointer;
+} row;
 
 typedef enum{
   TYPE_UINT = 4,
@@ -65,12 +64,58 @@ void* retreive(mutableArray* mArray, uint16_t index){
   return targetMemory;
 }
 
+typedef struct node{
+  void* mData;
+  dataType mType;
+  struct node* left;
+  struct node* right;
+} node;
+
+node* initNode(void* data, dataType type){
+  node* tempNode;
+  tempNode->mData = malloc(type);
+  tempNode->mType = type;
+  return tempNode;
+}
+
+//I think I understand why linkedlist is interesting, I will have raw pointers for other nodes from just this node
+//So here's what I'll do, I will keep going left of the rootnode until I find an empty Node, same with right 
+void addNode(node* rootNode, void* data, dataType type, char* direction){
+  node* newNode = initNode(data, type);
+  if(direction == 0){ //this node is to the left of the other node
+    node* currentNode = rootNode;
+    node* tempNode = currentNode->left;
+    while(tempNode!=NULL){
+      currentNode = tempNode;
+      tempNode = currentNode->left;
+    }
+    newNode->right = currentNode;
+    currentNode->left = newNode;
+    return;
+  }
+  node* currentNode = rootNode;
+  node* tempNode = currentNode->right;
+  while(tempNode!=NULL){
+    currentNode = tempNode;
+    tempNode = currentNode->right;
+  }
+    newNode->left= currentNode;
+    currentNode->right = newNode;
+  return;
+}
+
+void deleteNode();//Main thing is how to delete it? do I take a value? check value? do I just delete by index??? 
+
+
 int main(){
   mutableArray* arr = initArray(TYPE_UINT);
   resize(arr, 10);
   int value = 2000;
+  int value2 = 3000;
   insert(arr, &value, 5);
   int* x = (int*)retreive(arr, 5);
   printf("The retrieved value is: %d", *x); //dereferencing the pointer
+  insert(arr, &value2, 5);
+  printf("another value is: %d", *(int*)retreive(arr, 5));
   return 0;
 }
